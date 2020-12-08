31aadb2d734289b985144769c3c8a9e3fab8636d
Gzim <gzimasanni@gmail.com>
2020-12-08 19:06:50 +0100

    following changes were made based on reviews: Removed log, tmp and node_mudoles folders and add them to my gitignore Add a redirection path in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def signed_in_only!
+    # current_user in ApplicationController must be defined as helper method to make it work
+    redirect_to  new_user_session_path unless current_user
+  end
 end
